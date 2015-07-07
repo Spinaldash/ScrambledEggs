71cf91ebfa62b033dde29fde1483b31812220514
Spinaldash <blueavenger@gmail.com>
2015-07-07 16:33:10 -0700

    impementing timer controls

--- client/views/gameBoard/gameBoard.js
+++ client/views/gameBoard/gameBoard.js
@@ -94,4 +94,18 @@ angular.module('kensu')
     }
     return false;
   };
+
+  $scope.startTimer = function(){
+    $scope.$broadcast('timer-start');
+    $scope.timerRunning = true;
+  }
+  $scope.stopTimer = function (){
+    $scope.$broadcast('timer-stop');
+    $scope.timerRunning = false;
+  };
+
+  $scope.$on('timer-stopped', function (event, data){
+      console.log('Timer Stopped - data = ', data);
+  });
+
 });
