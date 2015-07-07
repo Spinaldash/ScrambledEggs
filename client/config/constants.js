9fe0f312b2ac7c27f420d5361aff9d15314584ae
Spinaldash <blueavenger@gmail.com>
2015-07-06 17:00:16 -0700

    intializing wordlist

--- client/config/constants.js
+++ client/config/constants.js
@@ -3,4 +3,4 @@
 angular.module('kensu')
 .constant('firebaseUrl', 'https://__________.firebaseio.com/')
 // .constant('nodeUrl', 'http://localhost:8000');
-.constant('tenWordsUrl', 'http://api.wordnik.com:80/v4/words.json/randomWords?hasDictionaryDef=true&minCorpusCount=0&maxCorpusCount=-1&minDictionaryCount=1&maxDictionaryCount=-1&minLength=5&maxLength=-1&limit=10&api_key=a2a73e7b926c924fad7001ca3111acd55af2ffabf50eb4ae5');
+.constant('tenWordsUrl', 'http://api.wordnik.com:80/v4/words.json/randomWords?hasDictionaryDef=true&minCorpusCount=0&maxCorpusCount=-1&minDictionaryCount=1&maxDictionaryCount=-1&minLength=5&maxLength=7&limit=10&api_key=a2a73e7b926c924fad7001ca3111acd55af2ffabf50eb4ae5');
--- client/views/gameBoard/gameBoard.js
+++ client/views/gameBoard/gameBoard.js
@@ -2,9 +2,20 @@
 
 angular.module('kensu')
 .controller('GameCtrl', function($rootScope, $scope, $state, $http, Wordlist){
+
+  // Initialize the game by hitting the API and saving the wordList
   Wordlist.initialize()
     .then(function(data){
-      console.log(data);
+      console.log(data.data);
+      $scope.wordList = data.data.map(function(wordObject){
+        return wordObject.word
+      });
+      console.log($scope.wordList);
     })
+
+
+  function serveWord(wordsArray){
+    $scope.onDeck = wordsArray.shift();
+  }
 
 });
